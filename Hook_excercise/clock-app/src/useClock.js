6cc12493522add4854240d4b4346666109bc6044
phihungds <phihunds1@gmail.com>
2022-01-11 17:42:59 +0700

    xong clock

--- Hook_excercise/clock-app/src/useClock.js
+++ Hook_excercise/clock-app/src/useClock.js
@@ -18,7 +18,14 @@ function Clock() {
 
         setTime(`${hours}:${minutes}:${seconds}`)
         
-        let days = []
+        let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ]
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        let toDay = days[date.getDay()]
+        let dateNum = date.getDate()
+        let month = months[date.getMonth()]
+        let year = date.getFullYear()
+
+        setDay(`${toDay}, ${dateNum} ${month} ${year}`)
         }
         setInterval(function () {
             updateTime();
@@ -31,6 +38,7 @@ function Clock() {
         <div style={{textAlign:'center'}}>
             <div>
                 <h1>What time is it ?</h1>
+                <h2>{day}</h2>
                 <h2>{time}</h2>
             </div>
         </div>
