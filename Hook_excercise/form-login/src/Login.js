10c29e590747630031429d8c16cb31f21442cbaa
phihungds <phihunds1@gmail.com>
2022-01-12 21:34:28 +0700

    tinh gọn

--- Hook_excercise/form-login/src/Login.js
+++ Hook_excercise/form-login/src/Login.js
@@ -1,17 +1,11 @@
 import { useState } from 'react';
 
 function Login() {
-    const [account, setAccount] = useState({
-        username: '',
-        password: ''
-    })
-
-    const updateAccount = (key, value) => {
-        let loginAccount = {...account,[key]: value  }
-        setAccount(loginAccount)
-    }
+    const [username, setUsername] = useState('')
+    const [pass, setPass] = useState('')
+    
     const displayAcc = ()=> {
-        document.getElementById('display').innerHTML = `<h3>Your account: </h3>  <h4>Username: ${account.username}</h4><h4>Password: ${account.password}</h4>`
+        document.getElementById('display').innerHTML = `<h3>Your account: </h3>  <h4>Username: ${username}</h4><h4>Password: ${pass}</h4>`
     }
     return (
         <div>
@@ -20,13 +14,13 @@ function Login() {
                     type='text'
                     placeholder='Enter your username'
                     onChange={
-                        (e) => { updateAccount('username', e.target.value) }} />
+                        (e) => { setUsername(e.target.value) }} />
                 <br />
                 <input
                     type='password'
                     placeholder='Enter your password'
                     onChange={
-                        (e) => { updateAccount('password', e.target.value) }} /> <br />
+                        (e) => { setPass(e.target.value) }} /> <br />
                 <button onClick={displayAcc}>Login</button>
             </div>
             <div id='display'>
