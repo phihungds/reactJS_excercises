34fc57616ab75a321a363695f698d91ff80103c1
phihungds <phihunds1@gmail.com>
2022-02-04 20:03:19 +0700

    add

--- Form_excercise/health-declaration/src/App.js
+++ Form_excercise/health-declaration/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { Formik } from 'formik'
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const genderList = [
+    { label: 'Nam', value: 'male' },
+    { label: 'Nữ', value: 'female' }]
+  const [form, setForm] = useState({})
+  const handleChange = (event) => {
+    let value = event.target.type ==='checkbox' ? form[event.target.name] : event.target.value;
+    setForm({...form, [event.target.name]: value})
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
   );
 }
 
-export default App;
