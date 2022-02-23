49d8a481e9db99c32a466acdc84b88db8049b68b
phihungds <phihunds1@gmail.com>
2022-02-23 21:10:44 +0700

    xong thuc hanh 1

---
+++ Test_excercise/thuc-hanh-1-2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const value = 0
+  return (
+    <div className="App">
+      <h1>Default Value</h1>
+      <div className="default-value">{value}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Test_excercise/thuc-hanh-1-2/src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+
+describe('App component', () =>{
+    it('Start with default value', () => {
+        const component = shallow(<App />);
+        const defaultValue = component.find('div.default-value').text();
+        expect(defaultValue).toEqual('0')
+    })
+})
