1a1213d2ad1109ee00551b56af50bd58d1907909
phihungds <phihunds1@gmail.com>
2022-02-24 20:30:01 +0700

    thuc hanh 2

--- Test_excercise/thuc-hanh-1-2/src/App.js
+++ Test_excercise/thuc-hanh-1-2/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
-  const value = 0
+  const [value, setValue] = useState(0)
   return (
     <div className="App">
-      <h1>Default Value</h1>
-      <div className="default-value">{value}</div>
+      <h1> Value</h1>
+      <div className="value">{value}</div>
+      <button className="increase" onClick={()=>setValue(value + 1)}>Increase</button>
+      <button className="decrease" onClick={()=>setValue(value - 1)}>Decrease</button>
     </div>
   );
 }
--- Test_excercise/thuc-hanh-1-2/src/App.test.js
+++ Test_excercise/thuc-hanh-1-2/src/App.test.js
@@ -1,12 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import App from './App';
 
 describe('App component', () =>{
     it('Start with default value', () => {
         const component = shallow(<App />);
-        const defaultValue = component.find('div.default-value').text();
-        expect(defaultValue).toEqual('0')
+        const defaultValue = component.find('div.value').text();
+        expect(defaultValue).toEqual('0');
+    })
+    it('Increase value by 1 on click', () => {
+      const component = shallow(<App />)
+      const incrementBtn = component.find('button.increase')
+      incrementBtn.simulate('click')
+      const result = component.find('.value').text()
+      expect(result).toEqual("1")
+    })
+    it('Decrease value by 1 on click', () => {
+      const component = shallow(<App />)
+      const decrementBtn = component.find('button.decrease')
+      decrementBtn.simulate('click')
+      const result = component.find('.value').text()
+      expect(result).toEqual('-1')
     })
 })
